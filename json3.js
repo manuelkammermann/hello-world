6da7ba7cb4912b30de779e57eeabdc3a73f1d1d3
manuelkammermann <35066733+manuelkammermann@users.noreply.github.com>
2018-06-26 13:44:20 +0200

    Update and rename json2.js to json3.js

--- json3.js
+++ json3.js
@@ -1,3 +1,8 @@
+var script = document.createElement('script');
+ 
+script.src = '//code.jquery.com/jquery-1.11.0.min.js';
+document.getElementsByTagName('head')[0].appendChild(script); 
+
 $(function(){
     var $page = jQuery.url.attr("file");
     $('ul.navigation li a').each(function(){
